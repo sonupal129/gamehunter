263c8d70ac4215d8ce671b5f1e9531b3f454525b
Sonu Pal <connectwithgamehunter@gmail.com>
2019-02-07 18:26:12 +0530

    Added Extra Settings Details

--- gamehunter/settings.py
+++ gamehunter/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = '@aju5^l81trvsenrh%3lw^c$6i1_vnb^zqxpd&2$j#$!03s_ao'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -80,7 +78,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'gamehunter.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
@@ -90,7 +87,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
@@ -110,7 +106,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
@@ -123,7 +118,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
@@ -179,3 +173,8 @@ SLACK_TOKEN = 'xoxb-402668290307-541515247091-5kxEPnxOQ0dQSLy334WoENDE'
 
 INSTAMOJO_API_KEY = "test_830b282750b49d50fb88d7e72c9"
 INSTAMOJO_AUTH_TOKEN = "test_f544367f6405aa31283a4605731"
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
