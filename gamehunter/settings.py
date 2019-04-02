a8d9eaec8d360594f03dd19569f545c92242bc19
Sonu Pal <connectwithgamehunter@gmail.com>
2019-04-02 17:38:24 +0530

    minor fix

--- gamehunter/settings.py
+++ gamehunter/settings.py
@@ -12,11 +12,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
-try:
-    from .local_settings import *
-except ImportError:
-    pass
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -214,3 +209,7 @@ CACHES = {
 EXCLUDE_FROM_MINIFYING = ('url-optimize/sitemap.xml/', 'cki39vbl3/admin/')
 
 
+try:
+    from .local_settings import *
+except ImportError:
+    pass
